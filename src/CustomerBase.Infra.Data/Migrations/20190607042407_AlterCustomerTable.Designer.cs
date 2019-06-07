2e9ada9d82002efdd7f88efd4563fece18d32af8
Jamil Tadeu Zazu <jamilzazu@hotmail.com>
2019-06-07 00:24:33 -0400

    AlterCustomerTable

--- src/CustomerBase.Infra.Data/Migrations/20190607042407_AlterCustomerTable.Designer.cs
+++ src/CustomerBase.Infra.Data/Migrations/20190607042407_AlterCustomerTable.Designer.cs
@@ -4,14 +4,16 @@ using CustomerBase.Infra.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CustomerBase.Infra.Data.Migrations
 {
     [DbContext(typeof(CustomerContext))]
-    partial class CustomerContextModelSnapshot : ModelSnapshot
+    [Migration("20190607042407_AlterCustomerTable")]
+    partial class AlterCustomerTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/CustomerBase.Infra.Data/Migrations/20190607042407_AlterCustomerTable.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CustomerBase.Infra.Data.Migrations
+{
+    public partial class AlterCustomerTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Phone",
+                table: "Customer",
+                type: "varchar(20)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(150)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Customer",
+                type: "varchar(150)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Document",
+                table: "Customer",
+                type: "varchar(20)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(150)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Phone",
+                table: "Customer",
+                type: "varchar(150)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(20)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Customer",
+                type: "varchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(150)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Document",
+                table: "Customer",
+                type: "varchar(150)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(20)",
+                oldNullable: true);
+        }
+    }
+}
